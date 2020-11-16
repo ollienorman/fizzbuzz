78b2c00d951c209403504b4c48f1d4e6becd6460
Will Dixon <dixon.will1@gmail.com>
2020-11-16 14:03:42 +0000

    Refactored fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,6 @@
 def fizzbuzz(number)
-    if number % 3 == 0 && number % 5 == 0
-        return "fizzbuzz"
-    elsif number % 3 == 0
-        return "fizz"
-    elsif number % 5 == 0
-        return "buzz"
-    else
-        return number
-    end
+    return "fizzbuzz" if number % 15 == 0
+    return "fizz" if number % 3 == 0 
+    return "buzz" if number % 5 == 0
+    return number
 end
